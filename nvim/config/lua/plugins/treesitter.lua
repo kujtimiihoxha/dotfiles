ea771a3d837379711e6c0d604a3b5983118675f4
kujtimiihoxha <kujtimii.h@gmail.com>
2025-10-16 12:38:40 +0200

    small patch

--- nvim/config/lua/plugins/treesitter.lua
+++ nvim/config/lua/plugins/treesitter.lua
@@ -9,6 +9,8 @@ return {
     ensure_installed = {
       "lua",
       "vim",
+      "gotmpl",
+      "helm",
       -- add more arguments for adding more treesitter parsers
     },
   },
--- nvim/config/lua/polish.lua
+++ nvim/config/lua/polish.lua
@@ -1,5 +1,12 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+vim.filetype.add {
+  extension = {
+    gotmpl = "gotmpl",
+  },
+  pattern = {
+    [".*/.*%.tpl"] = "helm",
+    [".*/templates/.*%.ya?ml"] = "helm",
+  },
+}
