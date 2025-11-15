2f2b8b196d828608a2d2b6d33ad9eba95a67ba1a
Kujtim Hoxha <kujtimii.h@gmail.com>
2025-11-15 21:54:40 +0100

    chore: some changes

--- tmux/install.sh
+++ tmux/install.sh
@@ -2,4 +2,4 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 ln -fs ~/.dotfiles/tmux/config ~/.config/tmux
 
-sh ~/.tmux/plugins/tpm/scripts/install_plugins.sh
+sh ~/.tmux/plugins/tpm/scripts/install_plugins.sh 
