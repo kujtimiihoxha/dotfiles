85f331a23962174773c3389854c5c301edc8765a
Kujtim Hoxha <kujtimii.h@gmail.com>
2025-11-16 18:34:35 +0100

    chore: more stuff

--- go/install.sh
+++ go/install.sh
@@ -2,3 +2,4 @@ go install github.com/air-verse/air@latest
 go install github.com/caarlos0/svu/v3@latest
 go install github.com/go-task/task/v3/cmd/task@latest
 go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v2.6.2
+go install golang.org/x/tools/gopls@latest
